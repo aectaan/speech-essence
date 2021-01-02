752e81198b827e230f6d75fe76ce4431a95869a3
Egor Markov <emarkov@notanotherone.com>
2021-01-02 13:32:57 +0300

    tested build

--- INSTALL_PREREQUISITES.sh
+++ INSTALL_PREREQUISITES.sh
@@ -1,21 +1,24 @@
 #!/bin/bash
 
 #This instruction provided by https://github.com/wzhd/vosk-sys
+# Update dependencies
+git submodule init
+git submodule update
 #First install dependencies and build the speech recognition toolkit Kaldi, which Vosk is based on
 sudo apt-get install g++ automake autoconf unzip wget curl sox gfortran libtool subversion python2.7 python3 zlib1g-dev
 #Install Rust
 curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
 rustup update stable
-#Иuild Kaldi
+#Build Kaldi
 cd kaldi/tools
-make
-#Install linear algebra libraries (uncomment preferred one), default - ATLAS
+#Install linear algebra libraries, default - ATLAS
 #Option 1: ATLAS installation
 sudo apt-get install libatlas-base-dev
 #Option 2: OpenBLAS installation
 ./extras/install_openblas.sh
 #Option 3: Intel-mkl installation
 ./extras/install_mkl.sh
+make
 #Kaldi compilation
 cd ../src
 #Configure with mathlib set to the linear algebra library used:
@@ -26,6 +29,4 @@ cd ..
 echo "export KALDI_ROOT=$(pwd)" >> $HOME/.bashrc
 source ~/.bashrc
 cd ../vosk-sys
-git submodule init
-git submodule update
-cargo build
+cargo build --release
