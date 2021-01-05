605d01c8227c7e693f0e4c51ae078bab51691a52
Egor Markov <emarkov@notanotherone.com>
2021-01-06 00:05:47 +0300

    [+] some error handling, dispatch to modules

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,10 @@
-use riff_wave::WaveReader;
-use scan_dir::{ScanDir, Walker};
+mod wav_stt;
+
+use scan_dir::ScanDir;
+use std::error::Error;
 use std::ffi::OsStr;
-use std::fs::{create_dir, create_dir_all, File};
-use std::io::{BufReader, Write};
-use std::path::{Path, PathBuf};
+use std::path::PathBuf;
 use structopt::StructOpt;
-use vosk::{Model, Recognizer, SpeakerModel, SpeakerRecognizer};
 
 #[derive(Debug, StructOpt)]
 #[structopt(
@@ -27,73 +26,7 @@ struct InputArgs {
     output_path: PathBuf,
 }
 
-fn read_sample(r: &mut WaveReader<BufReader<File>>, buf: &mut [i16]) -> usize {
-    let mut i = 0;
-    for _ in 0..buf.len() {
-        match r.read_sample_i16() {
-            Ok(s) => {
-                buf[i] = s;
-                i += 1;
-            }
-            Err(e) => {
-                println!("{:?}", e);
-                break;
-            }
-        }
-    }
-    i
-}
-
-fn process_wav(
-    input_path: PathBuf,
-    recognition_model: &PathBuf,
-    speaker_model: Option<&PathBuf>,
-    output_path: PathBuf,
-) {
-    let input_file = File::open(&input_path).unwrap();
-    let mut text = File::create(output_path).unwrap();
-
-    let reader = BufReader::new(input_file);
-    let mut wave_reader = WaveReader::new(reader).unwrap();
-    let fmt = &wave_reader.pcm_format;
-
-    let mut buf = [0; 1024];
-    let recognition_model = Model::new(recognition_model).unwrap();
-    if speaker_model.is_some() {
-        let speaker_model = SpeakerModel::new(speaker_model.unwrap()).unwrap();
-        let mut speaker_recognizer =
-            SpeakerRecognizer::new(&recognition_model, &speaker_model, fmt.sample_rate as f32);
-    }
-
-    let mut speech_recognizer = Recognizer::new(&recognition_model, fmt.sample_rate as f32);
-    let mut last_part = String::new();
-
-    loop {
-        let n = read_sample(&mut wave_reader, &mut buf);
-        if n == 0 {
-            let result = speech_recognizer.final_result();
-            writeln!(text, "{}", result.text).unwrap();
-            // println!("Final result: {}", result.text);
-            break;
-        } else {
-            let completed = speech_recognizer.accept_waveform(&buf[..n]);
-            if completed {
-                let result = speech_recognizer.final_result();
-                writeln!(text, "{}", result.text).unwrap();
-            // println!("Result: {}", result.text);
-            } else {
-                let result = speech_recognizer.partial_result();
-                if result.partial != last_part {
-                    last_part.clear();
-                    last_part.insert_str(0, &result.partial);
-                    // println!("Partial: {}", result.partial);
-                }
-            }
-        }
-    }
-}
-
-fn main() {
+fn main() -> Result<(), Box<dyn Error>> {
     let args: InputArgs = InputArgs::from_args();
     let mut files = Vec::new();
     let recognition_model = args.recognition_model;
@@ -101,20 +34,23 @@ fn main() {
     let output_path = args.output_path;
 
     if !output_path.exists() {
-        create_dir_all(output_path.clone()).unwrap();
+        std::fs::create_dir_all(output_path.clone())?;
     }
 
     if args.inputs.is_dir() {
-        ScanDir::files()
-            .walk(args.inputs, |iter| {
-                for (entry, name) in iter {
-                    files.push((
-                        entry.path(),
-                        String::from(entry.path().file_stem().unwrap().to_str().unwrap()),
-                    ));
-                }
-            })
-            .unwrap();
+        let walk_res = ScanDir::files().walk(args.inputs, |iter| {
+            for (entry, name) in iter {
+                files.push((
+                    entry.path(),
+                    String::from(entry.path().file_stem().unwrap().to_str().unwrap()),
+                ));
+            }
+        });
+        if let Err(errors) = walk_res {
+            for e in errors {
+                eprintln!("Files discovering error {}", e);
+            }
+        }
     } else {
         let name = String::from(args.inputs.file_stem().unwrap().to_str().unwrap());
         files.push((args.inputs, name));
@@ -130,10 +66,11 @@ fn main() {
         match ext {
             Some("wav") => {
                 println!("processing file {}", f.0.to_str().unwrap());
-                process_wav(f.0, &recognition_model, speaker_model.as_ref(), filename);
+                wav_stt::process(f.0, &recognition_model, speaker_model.as_ref(), filename)?;
             }
             Some(_) => eprintln!("Unsupported file {}", f.0.to_str().unwrap()),
             None => eprintln!("File has no extension {}", f.0.to_str().unwrap()),
         }
     }
+    Ok(())
 }
