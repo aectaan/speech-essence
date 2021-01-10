ab3200722001a3bc6bc170a35e200d13d29ba541
Egor Markov <emarkov@notanotherone.com>
2021-01-10 17:39:56 +0300

    fail somewhere in the processing

--- src/main.rs
+++ src/main.rs
@@ -15,16 +15,16 @@ use structopt::StructOpt;
 )]
 struct InputArgs {
     #[structopt(short = "f", long = "file")]
-    /// Path to input audio file. Only mono WAV is supported at the moment
+    /// Path to input audio file or folder with files. WAV, MP3 and OPUS is supported at the moment
     inputs: PathBuf,
     #[structopt(short = "m", long = "model")]
     /// Path to the model. Get one from https://alphacephei.com/vosk/models and unpack.
     recognition_model: PathBuf,
     #[structopt(short = "s", long = "speaker_model")]
-    /// Path to the speaker identification model. Available at https://alphacephei.com/vosk/models
+    /// Path to the speaker identification model. Not implemented at the moment. Available at https://alphacephei.com/vosk/models
     speaker_model: Option<PathBuf>,
     #[structopt(short = "o", long = "output")]
-    /// Path to the output file. Decoded text will be routed to stdout if path is not provided.
+    /// Output directory.
     output_path: PathBuf,
 }
 
@@ -73,6 +73,10 @@ fn main() -> Result<(), Box<dyn Error>> {
             Some("mp3") => {
                 println!("processing file {}", f.0.to_str().unwrap());
                 mp3_stt::process(f.0, &recognition_model, speaker_model.as_ref(), filename)?;
+            }
+            Some("opus") => {
+                println!("processing file {}", f.0.to_str().unwrap());
+                opus_stt::process(f.0, &recognition_model, speaker_model.as_ref(), filename)?;
             }
             Some(_) => eprintln!("Unsupported file {}", f.0.to_str().unwrap()),
             None => eprintln!("File has no extension {}", f.0.to_str().unwrap()),
--- src/opus_stt.rs
+++ src/opus_stt.rs
@@ -1,6 +1,8 @@
-
+use std::fs::File;
 use std::path::PathBuf;
 use std::result::Result::Ok;
+use std::io::{Write, Read};
+use vosk::{Model, Recognizer, SpeakerModel};
 
 pub fn process(
     input_path: PathBuf,
@@ -8,27 +10,89 @@ pub fn process(
     speaker_model_path: Option<&PathBuf>,
     output_path: PathBuf,
 ) -> Result<(), Box<dyn std::error::Error>> {
-    let mut  file = opusfile::OggOpusFile::from_slice(input_path.to_str().unwrap().as_bytes())?;
+    let mut opus = File::open(input_path)?;
+    let mut file_raw:Vec<u8> = Vec::new();
+    opus.read_to_end(&mut file_raw)?;
+    let mut cursor = std::io::Cursor::new(file_raw.as_slice());
+    let mut file = opusfile::OggOpusFile::from_read(&mut cursor).unwrap();
     let head = file.head(None).unwrap();
     let sample_rate = head.input_sample_rate;
-    let channels = head.channel_count;
+    let channels = head.channel_count as usize;
 
-    let mut data: Vec<i16> = Vec::new();
+
+    let mut data = Vec::new();
+    for _ in 0..channels {
+        data.push(Vec::new());
+    }
 
     match channels {
-        1 => while let Ok(count) = file.read(&mut *data, None){
-            if count ==0{
-                break;
-            }
-        },
-        2 => {
-            while let Ok(count) =file.read_stereo(&mut data){
-                if count ==0{
+        1 => {
+            let mut buf : [f32; 11520] = [0.0; 11520];
+            while let Ok(count) = file.read_float(&mut buf[..], None) {
+                if count == 0 {
                     break;
                 }
+                data[0].extend_from_slice(&mut buf);
             }
-        },
+        }
+        2 => {
+            let mut buf : [f32; 11520] = [0.0;11520];
+            while let Ok(count) = file.read_float_stereo(&mut buf[..]) {
+                if count == 0 {
+                    break;
+                }
+                for channel in 0..channels {
+                    let frame_data = buf.iter().skip(channel as usize).step_by(channels as usize);
+                    data[channel].extend(frame_data);
+                }
+            }
+        }
         _ => panic!("wrong channel number {}", channels),
+    };
+
+    for channel in 0..channels {
+        // Attach provided recognition model
+        let recognition_model = match Model::new(recognition_model_path) {
+            Ok(model) => model,
+            Err(_) => panic!(
+                "no valid recognition model at {}",
+                recognition_model_path.to_str().unwrap()
+            ),
+        };
+        // Create recognizer.
+        let mut recognizer = match speaker_model_path {
+            Some(path) => {
+                let _speaker_model = match SpeakerModel::new(path) {
+                    Ok(model) => model,
+                    Err(_) => panic!(
+                        "no valid recognition model at {}",
+                        recognition_model_path.to_str().unwrap()
+                    ),
+                };
+                Recognizer::new(&recognition_model, sample_rate as f32)
+                // Not implemented yet
+                //SpeakerRecognizer::new(&recognition_model, &speaker_model, sample_rate)
+            }
+            None => Recognizer::new(&recognition_model, sample_rate as f32),
+        };
+
+        let mut name = output_path.clone();
+        name = name
+            .with_file_name(
+                name.file_stem().unwrap().to_str().unwrap().to_owned()
+                    + "_channel_"
+                    + channel.to_string().as_str(),
+            )
+            .with_extension("txt");
+        let mut text = File::create(name).unwrap();
+        let completed = recognizer.accept_waveform_f32(&data[channel]);
+        if completed {
+            let result = recognizer.result();
+            writeln!(text, "{}", result.text)?;
+        } else {
+            let result = recognizer.final_result();
+            writeln!(text, "{}", result.text)?;
+        }
     }
     Ok(())
 }
